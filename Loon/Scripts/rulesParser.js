5661865a31c52feafbd9f64dffb00f4f0ca2bc49
Henry Leo <159218379+HiHenryLeo@users.noreply.github.com>
2024-04-26 10:36:07 +0800

    Update rulesParser.js

--- Loon/Scripts/rulesParser.js
+++ Loon/Scripts/rulesParser.js
@@ -1,5 +1,6 @@
 let obj = $response.body;
 obj = obj.replace(/HOST/gi,'DOMAIN');
-obj = obj.replace(/IP6-CIDR/gi,'IP-CIDR6');
+//obj = obj.replace(/IP6-CIDR/gi,'IP-CIDR6');
+obj = obj.replace(/IP/gi,'#IP');
 
 $done({body:obj}); 
